789f3e9479b4448c1f54fd20d4b3364a549ddd9a
Nathaniel Panares <npanares4@gmail.com>
2026-03-13 15:12:10 +0800

    Changed spacing of success message

--- components/ui/alert.jsx
+++ components/ui/alert.jsx
@@ -7,7 +7,7 @@ function Alert({ className, ...props }) {
     <div
       role="alert"
       className={cn(
-        "relative grid w-full grid-cols-[0_1fr] items-start gap-x-3 rounded-lg border px-4 py-3 text-sm shadow-sm [&>svg]:size-4 [&>svg]:translate-y-0.5",
+        "relative grid w-full grid-cols-[0_1fr] items-start gap-[20px] rounded-lg border px-4 py-3 text-sm shadow-sm [&>svg]:size-4 [&>svg]:translate-y-0.5 justify-center",
         className
       )}
       {...props}
@@ -16,7 +16,7 @@ function Alert({ className, ...props }) {
 }
 
 function AlertTitle({ className, ...props }) {
-  return <h5 className={cn("mb-1 font-semibold leading-none tracking-tight", className)} {...props} />;
+  return <h5 className={cn("font-semibold leading-none tracking-tight", className)} {...props} />;
 }
 
 function AlertDescription({ className, ...props }) {
